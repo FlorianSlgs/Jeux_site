70a692cc6bf9f3cbc0fa2b6213c114e37c5e2464
flodk <flo.dkniman@gmail.com>
2025-02-12 15:25:40 +0100

    prerender

--- server/app.js
+++ server/app.js
@@ -10,10 +10,12 @@ const server = http.createServer(app);
 app.use(cors());
 const io = new Server(server, { cors: { origin: "*" } });
 
+const prerender = require('prerender-node');
+
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
-app.use(require('prerender-node').set('prerenderToken', 'mzZBfjnhtM4vNul3mZ53'));
+app.use(prerender.set('prerenderToken', 'mzZBfjnhtM4vNul3mZ53'));
 
 // Connexion à MongoDB Atlas
 mongoose.connect(MONGO_URI)
